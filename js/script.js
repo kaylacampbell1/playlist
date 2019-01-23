a4fe8feeed76583321ce8bb263c846597621cbbd
Kayla Campbell <campbellkayla2003@gmail.com>
2019-01-23 22:42:46 +0000

    need fixing

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["Sunflower","Trip","Supernova","Thief"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
-
+var images_links = ["https://t2.genius.com/unsafe/220x220/https%3A%2F%2Fimages.genius.com%2Ff8df6f94846dc4a8f2e1b94d7abd1cb5.1000x1000x1.jpg", "https://static.spin.com/files/2018/08/Ella-Mai-Press-Photo_Photo-Credit_Jonathan-Mannion-1533309795-640x455.jpg", "http://cdn02.cdn.justjared.com/wp-content/uploads/headlines/2018/01/ansel-elgort-debuts-music-video-for-new-song-supernova.jpg", "https://cps-static.rovicorp.com/3/JPG_500/MI0004/188/MI0004188527.jpg?partner=allrovi.com"];
+var length = ["2:41", "3:38", "2:34", "3:40"];
+var links = ["https://www.youtube.com/watch?v=ApXoWvfEYVU", "https://www.youtube.com/watch?v=JfGD75vHWrU", "https://www.youtube.com/watch?v=Q-oM2BQ8Jow", "https://www.youtube.com/watch?v=ulAakKHUp5g"];
+var artists = ["Post Malone ft. Swae Lee", "Ella Mae", "Ansel Elgort", "Ansel Elgort"]
 
 
 function displaySongInfo(){
     // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
-
+    images_links.forEach(function(pic){
+        $("#images").append("<img src =\"" + pic + "\">");
+    });
+    
+    songs.forEach(function(name){
+       $("#songs").append("<div>" + name + "</div>");
+    });
+    
+    length.forEach(function(length){
+       $("#lengths").append("<div>" +length + "</div>");
+    });
+    
+    links.forEach(function(link){
+        $("links").append("<div>" + "<a href=" + link + ">" + "Listen To Song </a>" + "</div>");
+    });
+    
+    artists.forEach(function(artist){
+        $("artists").append("<div>" + artist + "</div>");
+    });
 }
 
 function emptySongInfo(){
